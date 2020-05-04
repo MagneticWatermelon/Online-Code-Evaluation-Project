85a5b22d4474451c7fda5398606275bad6579ceb
Oguzhan <fgtyhtyh@hotmail.com>
2020-05-04 19:46:01 +0300

    save session code

--- client/src/client/components/CodeArea/CodeArea.js
+++ client/src/client/components/CodeArea/CodeArea.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Editor from '@monaco-editor/react';
 
 
 export default function CodeArea() {
 
+    const editorRef = useRef();
+
+    function handleEditorDidMount(_, editor) {
+        editorRef.current = editor;
+        listenEditorChanges();
+    }
+
+    function listenEditorChanges() {
+        editorRef.current.onDidChangeModelContent(ev => {
+        sessionStorage.setItem('sessionCode', editorRef.current.getValue());
+        });
+    }
+
     return (
-        <Editor 
+        <Editor
+            value={sessionStorage.getItem('sessionCode')}
             theme='dark'
             language='java'
+            editorDidMount={handleEditorDidMount}
         />
     );
 }
