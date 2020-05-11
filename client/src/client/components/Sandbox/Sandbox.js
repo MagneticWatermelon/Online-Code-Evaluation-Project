bf08fab9160a200bc20e43bc9f6d4bbf59c05a90
Oguzhan <fgtyhtyh@hotmail.com>
2020-05-11 16:44:40 +0300

    fix code editor resizing issue

--- client/src/client/components/Sandbox/Sandbox.js
+++ client/src/client/components/Sandbox/Sandbox.js
@@ -1,18 +1,114 @@
-import React from 'react';
+import React , { useRef}from 'react';
 import SplitPane from 'react-split-pane';
 import Pane from 'react-split-pane'
 import ProblemArea from '../ProblemArea/ProblemArea';
-import CodeArea from '../CodeArea/CodeArea';
 import OutputArea from '../OutputArea/OutputArea';
-import './SandBox.css'
+import './SandBox.css';
+import Editor from '@monaco-editor/react';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 export default function Sandbox(props) {
+
+    const editorRef = useRef();
+
+    function handleEditorDidMount(_, editor) {
+        editorRef.current = editor;
+        
+        listenEditorChanges();
+        
+    }
+
+    function listenEditorChanges() {
+        editorRef.current.onDidChangeModelContent(ev => {
+            sessionStorage.setItem(props.sessionId, editorRef.current.getValue());
+        });
+    }
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          width: '100%',
+          height: '100%',
+        },
+      }));
+
+    const styles = useStyles();
+
+    const handleChange = (event) => {
+        editorRef.current.layout();
+    };
+
     return (
-        <SplitPane split='vertical'>
+        <SplitPane split='vertical' onChange={handleChange}>
             <ProblemArea />
-            <Pane minSize="10%" maxSize="33%">
-                <CodeArea sessionId={props.id}/>
+            <Pane minSize="10%">
+                <Editor
+                    value={sessionStorage.getItem(props.sessionId)}
+                    theme='dark'
+                    language='javascript'
+                    editorDidMount={handleEditorDidMount}
+                    width='100%'
+                    height='100%'
+                    options={
+        {                  "acceptSuggestionOnCommitCharacter": true,
+                            "acceptSuggestionOnEnter": "on",
+                            "accessibilitySupport": "auto",
+                            "autoIndent": false,
+                            "automaticLayout": true,
+                            "codeLens": true,
+                            "colorDecorators": true,
+                            "contextmenu": true,
+                            "cursorBlinking": "blink",
+                            "cursorSmoothCaretAnimation": false,
+                            "cursorStyle": "line",
+                            "disableLayerHinting": false,
+                            "disableMonospaceOptimizations": false,
+                            "dragAndDrop": false,
+                            "extraEditorClassName": "true",
+                            "fixedOverflowWidgets": false,
+                            "folding": true,
+                            "foldingStrategy": "auto",
+                            "fontLigatures": false,
+                            "formatOnPaste": false,
+                            "formatOnType": false,
+                            "hideCursorInOverviewRuler": false,
+                            "highlightActiveIndentGuide": true,
+                            "links": true,
+                            "mouseWheelZoom": false,
+                            "multiCursorMergeOverlapping": true,
+                            "multiCursorModifier": "alt",
+                            "overviewRulerBorder": true,
+                            "overviewRulerLanes": 2,
+                            "quickSuggestions": true,
+                            "quickSuggestionsDelay": 100,
+                            "readOnly": false,
+                            "renderControlCharacters": false,
+                            "renderFinalNewline": true,
+                            "renderIndentGuides": true,
+                            "renderLineHighlight": "all",
+                            "renderWhitespace": "none",
+                            "revealHorizontalRightPadding": 30,
+                            "roundedSelection": true,
+                            "rulers": [],
+                            "scrollBeyondLastColumn": 5,
+                            "scrollBeyondLastLine": true,
+                            "selectOnLineNumbers": true,
+                            "selectionClipboard": true,
+                            "selectionHighlight": true,
+                            "showFoldingControls": "mouseover",
+                            "smoothScrolling": false,
+                            "suggestOnTriggerCharacters": true,
+                            "wordBasedSuggestions": true,
+                            "wordSeparators": "~!@#$%^&*()-=+[{]}|;:'\",.<>/?",
+                            "wordWrap": "off",
+                            "wordWrapBreakAfterCharacters": "\t})]?|&,;",
+                            "wordWrapBreakBeforeCharacters": "{([+",
+                            "wordWrapBreakObtrusiveCharacters": ".",
+                            "wordWrapColumn": 80,
+                            "wordWrapMinified": true,
+                            "wrappingIndent": "none"
+                            }}
+                />                
             </Pane>
             <OutputArea />
         </SplitPane>
