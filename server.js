c159c28c6efb851d109a9eca1d20b2aef5e97561
Oguzhan <fgtyhtyh@hotmail.com>
2020-05-15 17:38:23 +0300

    fix minor stuff

--- server.js
+++ server.js
@@ -12,15 +12,14 @@ app.use(express.json());
 
 const uri = "mongodb+srv://OCES:ocesproject@mycluster-dvjk4.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
-console.log("saaaalih");
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Connected to DB');
 });
-addStudentToCourse("5eb3084223479d265c0f3cc7", "5eb1697c70abfb181839d82a", (err) => {
+/* addStudentToCourse("5eb3084223479d265c0f3cc7", "5eb1697c70abfb181839d82a", (err) => {
     if(err) return console.log(err);      
     //return console.log(res);
-})
+}) */
 
 /*
 getAssignments("5eb092c676d2394ed6ae3591", (err, res) => {
@@ -32,6 +31,6 @@ getAssignments("5eb092c676d2394ed6ae3591", (err, res) => {
    
 
 app.listen(port, () => {
-console.log('Server is listening...');
+console.log(`Server is listening at ${port}`);
 });
 
