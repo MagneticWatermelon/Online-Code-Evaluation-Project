78bab5c1da9cb236ee8f76216fe27afe376a5216
sadikcoban <sadik.coban@agu.edu.tr>
2020-06-03 04:36:24 +0300

    admin get delete create done

---
+++ client/src/admin/components/ui/CreateCourse.js
@@ -0,0 +1,285 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
+import Slide from "@material-ui/core/Slide";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Checkbox from "@material-ui/core/Checkbox";
+import TextField from "@material-ui/core/TextField";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Box from "@material-ui/core/Box";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Grid from "@material-ui/core/Grid";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: "relative",
+    backgroundColor: "#fa5788",
+    height: "100px",
+    marginTop: "80px",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+
+    flex: 1,
+  },
+  table: {
+    minWidth: 750,
+  },
+  progress: {
+    width: "100%",
+    height: "100%",
+    padding: "15% 20%",
+    marginLeft: "40em",
+  },
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+  paper: {
+    height: 140,
+    width: 200,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function CreateCourse(props) {
+  const classes = useStyles();
+  const [open, setOpen] = useState(true);
+  const [courseName, setCourseName] = useState("");
+  const [courseCode, setCourseCode] = useState("");
+  const [courseYear, setCourseYear] = useState("");
+  const [instructors, setInstructors] = useState([]);
+  const [courseCreateClicked, setCourseCreateClicked] = useState(false);
+  const [checked, setChecked] = React.useState([]);
+  const [fall, setFall] = useState(true);
+  const [spring, setSpring] = useState(false);
+
+  const handleTerm = () => {
+    setFall(!fall);
+    setSpring(!spring);
+  };
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  console.log(checked);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/user/get/all`, {
+        headers: { Authorization: `Bearer ${props.token}` },
+      })
+      .then((responseArr) => {
+        let myArr = responseArr.data;
+        let instArray = [];
+        myArr.map((user) => {
+          if (user.user_role === 1) instArray.push(user);
+        });
+        setInstructors(instArray);
+      });
+  }, []);
+
+  const handleCourseCreate = (event) => {
+    event.preventDefault();
+    let courseTerm;
+    if (fall && !spring) courseTerm = "Fall";
+    if (!fall && spring) courseTerm = "Spring";
+    let body = {
+      course_code: courseCode,
+      course_name: courseName,
+      year: courseYear,
+      term: courseTerm,
+      instructor_id: checked[0],
+    };
+    axios
+      .post("http://localhost:8080/course/create", body, {
+        headers: { Authorization: `Bearer ${props.token}` },
+      })
+      .then(function (response) {
+        console.log(response);
+        
+        if (checked.length > 1) {
+          console.log("eklemem")
+          let course_id = response.data.course_id;
+          checked.splice(0, 1);
+          let body = {
+            people: checked
+          };
+          axios.post(`http://localhost:8080/course/register/${course_id}`, body, {
+            headers: { Authorization: `Bearer ${props.token}` },
+          })
+            .then(function (response) {
+              console.log(response);
+            })
+            .catch(function (response) {
+              console.log(response);
+          })
+        }
+        handleClose();
+      })
+      .catch(function (response) {
+        console.log(response);
+        handleClose();
+      });
+
+    setCourseCreateClicked(!courseCreateClicked);
+    //handleClose();
+  };
+
+  return (
+    <div>
+      <Dialog
+        fullScreen="true"
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Create Course</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Create Course</DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Course Name"
+            fullWidth
+            type="email"
+            value={courseName}
+            onChange={(event) => setCourseName(event.target.value)}
+          />
+          <Box m={2} />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Course Code"
+            type="email"
+            fullWidth
+            value={courseCode}
+            onChange={(event) => setCourseCode(event.target.value)}
+          />
+          <Box m={2} />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Year"
+            type="number"
+            fullWidth
+            value={courseYear}
+            onChange={(event) => setCourseYear(event.target.value)}
+          />
+          <Box m={2} />
+          <FormControl component="fieldset">
+            <FormGroup aria-label="position" row>
+              <FormControlLabel
+                value="end"
+                control={
+                  <Checkbox
+                    onChange={handleTerm}
+                    checked={fall}
+                    defaultChecked
+                    color="primary"
+                  />
+                }
+                label="Fall"
+                labelPlacement="end"
+              />
+              <FormControlLabel
+                value="end"
+                control={
+                  <Checkbox
+                    onChange={handleTerm}
+                    checked={spring}
+                    color="primary"
+                  />
+                }
+                label="Spring"
+                labelPlacement="end"
+              />
+            </FormGroup>
+          </FormControl>
+          <Box m={2} />
+          <Typography variant="h6">Select Instructors</Typography>
+          <List dense className={classes.root}>
+            {instructors.map((value) => {
+              const labelId = `checkbox-list-secondary-label-${value._id}`;
+              return (
+                <Grid container className={classes.root} spacing={0}>
+                  <Grid item xs={12}>
+                    <Grid container spacing={2}>
+                      <Grid item>
+                        <ListItem key={value} button>
+                          <ListItemText id={labelId} primary={value.name} />
+
+                          <ListItemSecondaryAction>
+                            <Checkbox
+                              edge="end"
+                              onChange={handleToggle(value._id)}
+                              checked={checked.indexOf(value._id) !== -1}
+                              inputProps={{ "aria-labelledby": labelId }}
+                            />
+                          </ListItemSecondaryAction>
+                        </ListItem>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              );
+            })}
+          </List>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            component={Link}
+            to="/courses"
+            onClick={handleClose}
+            color="primary"
+          >
+            Cancel
+          </Button>
+          <Button
+            component={Link}
+            to="/courses"
+            onClick={handleCourseCreate}
+            color="primary"
+          >
+            Create Course
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
---
+++ client/src/admin/components/ui/CreateUser.js
@@ -0,0 +1,221 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
+import Slide from "@material-ui/core/Slide";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import TextField from "@material-ui/core/TextField";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Box from "@material-ui/core/Box";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Checkbox from "@material-ui/core/Checkbox";
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: "relative",
+    backgroundColor: "#fa5788",
+    height: "100px",
+    marginTop: "80px",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+
+    flex: 1,
+  },
+  table: {
+    minWidth: 750,
+  },
+  progress: {
+    width: "100%",
+    height: "100%",
+    padding: "15% 20%",
+    marginLeft: "40em",
+  },
+  root: {
+    display: "flex",
+  },
+  formControl: {
+    margin: theme.spacing(2),
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function CreateUser(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [state, setState] = React.useState({
+    student: true,
+    ta: false,
+    instructure: false,
+    admin: false,
+  });
+
+  const handleChange = (event) => {
+    setState({ [event.target.name]: event.target.checked });
+  };
+
+  const { student, ta, instructure, admin } = state;
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleCreate = (event) => {
+    console.log("creating user");
+    console.log(props.token);
+    console.log(name, mail, password);
+    let role;
+    if (student) role = 0;
+    if (ta) role = 1;
+    if (instructure) role = 2;
+    if (admin) role = 3;
+    event.preventDefault();
+    let body = {
+      name: name,
+      mail: mail,
+      role: role,
+      password: password,
+    };
+    axios
+      .post("http://localhost:8080/user/create", body, {
+        headers: { Authorization: `Bearer ${props.token}` },
+      })
+      .then(function (response) {
+        console.log(response);
+        handleClose();
+      })
+      .catch(function (response) {
+        console.log(response);
+        handleClose();
+      });
+  };
+
+  return (
+    <div>
+      <Dialog
+        fullScreen="true"
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Create User</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Create User</DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="User Name"
+            fullWidth
+            type="email"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <Box m={2} />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="User E-mail"
+            type="email"
+            fullWidth
+            value={mail}
+            onChange={(event) => setMail(event.target.value)}
+          />
+          <Box m={2} />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="User Password"
+            type="email"
+            fullWidth
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <Box m={2} />
+
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormLabel component="legend">Assign responsibility</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={student}
+                    onChange={handleChange}
+                    name="student"
+                  />
+                }
+                label="Student"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={ta} onChange={handleChange} name="ta" />
+                }
+                label="Teaching Assistant"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={instructure}
+                    onChange={handleChange}
+                    name="instructure"
+                  />
+                }
+                label="Instructure"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={admin}
+                    onChange={handleChange}
+                    name="admin"
+                  />
+                }
+                label="Admin"
+              />
+            </FormGroup>
+          </FormControl>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            component={Link}
+            to="courses"
+            color="primary"
+            token={props.token}
+          >
+            Cancel
+          </Button>
+          <Button
+            component={Link}
+            to="/students"
+            onClick={handleCreate}
+            color="primary"
+            token={props.token}
+           // disabled={()=> {this.name === ""}}
+          >
+            Create User
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
