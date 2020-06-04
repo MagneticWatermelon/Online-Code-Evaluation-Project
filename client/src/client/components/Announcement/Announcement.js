40959ace7764fe14fe5b2dd2849356b786fffddf
Oguzhan <fgtyhtyh@hotmail.com>
2020-06-04 19:50:47 +0300

    change instructor announcement page styling

--- client/src/client/components/Announcement/Announcement.js
+++ client/src/client/components/Announcement/Announcement.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { Avatar, Typography, Button } from '@material-ui/core';
+import { Avatar, Typography, Button, IconButton, Menu, MenuItem, Link } from '@material-ui/core';
 import { Link as RouterLink} from 'react-router-dom';
+import SettingsIcon from '@material-ui/icons/Settings';
+
 import { useSnackbar } from 'notistack';
 
 import axios from 'axios';
@@ -11,12 +13,22 @@ export default function Announcement(props) {
 
     const [announcement, setAnnouncement] = React.useState({title: '', instructor: {name: ''}, explanation: ''});
     const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
 
     const [id, setId] = React.useState(() => {
         let url = window.location.pathname;
         let id = url.split('/').pop();
         return id;
     })
+
+    const handleSettingsClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+    
+    const handleSettingsClose = () => {
+        setAnchorEl(null);
+    };
 
     useEffect(() => {
         let arr = props.announcements;
@@ -59,27 +71,7 @@ export default function Announcement(props) {
 
     return(
         <div className={styles.root}>
-            {props.role == 1  && 
-            (<div>
-                <Button
-                    variant='contained'
-                    color='primary'
-                    component={RouterLink}
-                    to={`/courses/${props.course.course_code}/announcement/update/${id}`}
-                >
-                    Update
-                </Button>
-                <Button
-                    variant='contained'
-                    color='secondary'
-                    component={RouterLink}
-                    to={`/courses/${props.course.course_code}/announcements`}
-                    onClick={handleDelete}
-                >
-                    Delete
-                </Button>
-            </div>)
-            }
+            
             <div className={styles.header}>
                 <Avatar />
                 <div className={styles.header_title}>
@@ -97,7 +89,48 @@ export default function Announcement(props) {
                         {announcement.instructor.name}
                     </Typography>
                 </div>
-
+                {props.role == 1 && 
+            (
+            <div>
+                <IconButton
+                    aria-controls="simple-menu" 
+                    aria-haspopup="true" 
+                    onClick={handleSettingsClick}
+                    color='secondary'
+                >
+                    <SettingsIcon />
+                </IconButton>
+                <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleSettingsClose}
+                        anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                        }}
+                    >
+                        <MenuItem
+                        component={RouterLink}
+                        to={`/courses/${props.course.course_code}/announcement/update/${id}`}
+                        >
+                        Update
+                        </MenuItem>
+                        <MenuItem 
+                        component={RouterLink}
+                        onClick={handleDelete}
+                        to={`/courses/${props.course.course_code}/announcements`}
+                        >
+                        Delete
+                        </MenuItem>
+                    </Menu>
+            </div>
+            )}
             </div>
             <div className={styles.content} dangerouslySetInnerHTML={{__html: announcement.explanation}}>
                 
